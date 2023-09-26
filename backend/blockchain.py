38af8bf66c283d4d5f4b1c10a44193ee37d59060
Arvind Raghu <41343538+ArvindR03@users.noreply.github.com>
2023-09-26 17:20:43 +0100

    created backend

---
+++ backend/blockchain.py
@@ -0,0 +1,118 @@
+import datetime
+import hashlib
+import json
+
+class BlockChain:
+
+    def __init__(self):
+        self.chain = []
+        self.genesis_block()
+
+    
+    # this is the hash operation that is used to check for proof of work
+    @staticmethod
+    def hash_op(new_proof, prev_proof):
+        return hashlib.sha256(
+                    str(new_proof ** 2 - prev_proof **2).encode()
+                ).hexdigest()
+        # should result in the first 4 digits of the hash being '0000'
+
+
+
+
+    # this is used to hash an entire block
+    @staticmethod
+    def hash_block(block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+
+
+
+    # this is used to check if a chain is valid
+    @staticmethod
+    def is_chain_valid(self, chain):
+        prev = chain[0]
+
+        for i in range(len(chain)):
+            block = chain[block]
+            if block["previous_hash"] != BlockChain.hash_block([prev]):
+                return False
+            
+            prev_proof = prev["proof"]  
+            curr_proof = block["proof"]
+
+            hash_op = BlockChain.hash_op(prev_proof, curr_proof)
+            if hash_op[:4] != '0000':
+                return False
+            
+            prev = block
+                
+        return True
+    
+
+    # TODO: make this harder to break
+    # this is the proof of work function for miners to be able to manipulate the chain
+    @staticmethod
+    def proof_of_work(prev_proof):
+        new_proof = 1
+        check_proof = False
+
+        while not check_proof:
+            hash_op = BlockChain.hash_op(new_proof, prev_proof)
+            if hash_op[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        
+        return new_proof
+    
+
+
+    # TODO: implement check here to verify
+    # this is used to add a block to the chain
+    def create_and_add_block(self, proof, prev_hash):
+        block = {
+            'index' : len(self.chain),
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': prev_hash
+        }
+
+        self.chain.append(block)
+        return block
+    
+
+
+    
+    # this is used to create and add the genesis block to the chain
+    def genesis_block(self):
+        return self.create_and_add_block(proof=1, prev_hash='0')
+    
+
+
+    
+    # this is a getter function to get the most recent block from the chain
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    
+# example use of the backend:
+    
+b = BlockChain()
+
+def client_add_block(b):
+
+    prev_block = b.get_previous_block()
+    prev_proof = prev_block['proof']
+
+    curr_proof = BlockChain.proof_of_work(prev_proof)
+    prev_hash = BlockChain.hash_block(prev_block)
+
+    new_block = b.create_and_add_block(curr_proof, prev_hash)
+
+for i in range(10):
+    client_add_block(b)
+
+for i in b.chain:
+    print(i)
